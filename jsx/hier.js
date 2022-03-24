48a2df7ffb2653eb918da7e7e8b5971c597fc4d6
alexseyka1 <alexseyka1@gmail.com>
2022-03-24 14:04:33 +0200

    Hier Parser and Hier Renderer complete

--- jsx/hier.js
+++ jsx/hier.js
@@ -1,168 +1,323 @@
-const Hier = {
-  createElement(tagName, attributes, children) {
-    const element = document.createElement(tagName)
+const Hier = (function () {
+  /**
+   * HIER RENDERER
+   */
+  const Hier = {
+    createElement(tagName, attributes, children) {
+      const element = document.createElement(tagName)
 
-    Object.entries(attributes || {}).map(([attribute, value]) => element.setAttribute(attribute, value))
+      Object.entries(attributes || {}).map(([attribute, value]) => (element[attribute] = value))
 
-    if (children) {
-      if (!Array.isArray(children)) children = [children]
-      children.map((child) => {
-        if (!child) return
-        element.appendChild(child instanceof Node ? child : document.createTextNode(child))
-      })
-    }
-    return element
-  },
-
-  createTextElement(value) {
-    return document.createTextNode(value)
-  },
-}
-
-const HierParser = {
-  PLACEHOLDER: `___PLACEHOLDER_${Date.now()}___`,
-
-  html(splits, ...values) {
-    const ast = HierParser.parseString(splits.join(HierParser.PLACEHOLDER), values)
-    const createElement = (astObj) => {
-      if (typeof astObj === "object") {
-        const children = (astObj.children || []).map((item) => createElement(item))
-        return Hier.createElement(astObj.tagName || astObj, astObj.props || null, children || [])
-      } else {
-        return Hier.createTextElement(astObj)
+      if (children) {
+        if (!Array.isArray(children)) children = [children]
+        children.map((child) => {
+          if (!child) return
+          element.appendChild(child instanceof Node ? child : document.createTextNode(child))
+        })
       }
-    }
+      return element
+    },
 
-    return ast.map((item) => createElement(item))
-  },
+    createTextElement(value) {
+      return document.createTextNode(value)
+    },
+
+    /**
+     * Renders root application component
+     * @param {*} className
+     * @param {*} rootNode
+     */
+    render(className, rootNode) {
+      const rootComponent = new className()
+
+      /**
+       * Create components from AST if needed
+       */
+      const createHierComponents = (ast) => {
+        const response = []
+        ast.map((astObj) => {
+          if (typeof astObj === "object" && astObj.hasOwnProperty("tagName")) {
+            /** Hier component found */
+            if (Util.getIsClass(astObj.tagName)) {
+              const hierComponent = new astObj.tagName(astObj.props || {})
+              createHierComponents(hierComponent.render()).map((nestedItem) => response.push(nestedItem))
+            } else if (astObj.children) {
+              /** Hier component NOT found - common tag */
+              astObj.children = createHierComponents(astObj.children)
+            }
+          }
+          /** Just text */
+          response.push(astObj)
+        })
+
+        return response
+      }
+
+      const components = createHierComponents(rootComponent.render())
+      const rendered = components.map((item) => HierParser.createElementFromAstObject(item))
+      rendered.map((element) => element && rootNode.appendChild(element))
+    },
+  }
+
+  const Util = {
+    getIsClass: function (callable) {
+      if (typeof callable !== "function") return false
+      return /^class *\w+.*{/.test(callable)
+    },
+  }
 
   /**
-   * Parses an HTML template string and returns AST
-   * @param {string} string html with placeholders
-   * @param {Array} values list of placeholders replacements
-   * @returns
+   * HIER PARSER
    */
-  parseString(string, values) {
-    const STATES = {
-      data: "date",
-      tag: "tag",
-    }
+  const HierParser = {
+    PLACEHOLDER: `___PLACEHOLDER_${Date.now()}___`,
 
-    let tokensStack = [],
-      openedTagsStack = []
+    ast(splits, ...values) {
+      return HierParser.parseString(splits.join(HierParser.PLACEHOLDER), values)
+    },
 
-    const insertElementToTree = (element) => {
-      /** If we have an opened tag we must append new token to his children list */
-      const lastOpenedTag = openedTagsStack[openedTagsStack.length - 1]
-      if (lastOpenedTag) {
-        if (!lastOpenedTag.children) lastOpenedTag.children = []
-        lastOpenedTag.children.push(element)
-      } else {
-        tokensStack.push(element)
-      }
-    }
-
-    let state = STATES.data
-    let str = string
-    let loop = 0
-
-    while (str.length) {
-      if (++loop >= 1000) {
-        console.error("Parsing error. So much loops.", { state, str })
-        break
-      }
-
-      switch (state) {
-        case STATES.data:
-          const dataMatched = str.match(/^[^<]+/)
-          if (dataMatched) {
-            const value = HierParser.parseValue(dataMatched[0].trim(), values)
-
-            if (value.length) {
-              insertElementToTree(value)
-            }
-            str = str.slice(dataMatched.index + dataMatched[0].length)
+    createElementFromAstObject(astObject) {
+      const createElement = (item) => {
+        if (typeof item === "object") {
+          const children = (item.children || []).map((item) => createElement(item))
+          let tagName = item.tagName || item
+          if (typeof tagName === "string") {
+            return Hier.createElement(tagName, item.props || null, children || [])
           }
-          state = STATES.tag
+        } else {
+          return Hier.createTextElement(item)
+        }
+      }
+
+      return createElement(astObject)
+    },
+
+    html(splits, ...values) {
+      const ast = HierParser.ast(splits, ...values)
+      return ast.map((item) => HierParser.createElementFromAstObject(item)).filter((item) => item)
+    },
+
+    /**
+     * Parses an HTML template string and returns AST
+     * @param {string} string html with placeholders
+     * @param {Array} values list of placeholders replacements
+     * @returns
+     */
+    parseString(string, values) {
+      const STATES = {
+        data: "date",
+        tag: "tag",
+      }
+
+      let tokensStack = [],
+        openedTagsStack = []
+
+      const insertElementToTree = (element) => {
+        /** If we have an opened tag we must append new token to his children list */
+        const lastOpenedTag = openedTagsStack[openedTagsStack.length - 1]
+        if (lastOpenedTag) {
+          if (!lastOpenedTag.children) lastOpenedTag.children = []
+          lastOpenedTag.children.push(element)
+        } else {
+          tokensStack.push(element)
+        }
+      }
+
+      let state = STATES.data
+      let str = string
+      let loop = 0
+
+      while (str.length) {
+        if (++loop >= 1000) {
+          console.error("Parsing error. So much loops.", { state, str })
           break
+        }
 
-        case STATES.tag:
-          const closeMatched = str.match(/^<\/\w+.*>/)
-          if (closeMatched) {
-            str = str.slice(closeMatched.index + closeMatched[0].length)
-            openedTagsStack.pop()
-          } else {
-            const tagMatched = str.match(/<(?<tagName>\w+)[^\s\/>]*\s?(?<attributes>[^>]*)>/)
-            if (tagMatched) {
-              const {
-                groups: { tagName, attributes },
-              } = tagMatched
-              const selfClosing = attributes.slice(-1) === "/"
-              const props = HierParser.parseProps(attributes, values)
-              const element = { tagName, props }
-              insertElementToTree(element)
+        switch (state) {
+          case STATES.data:
+            const dataMatched = str.match(/^[^<]+/)
+            if (dataMatched) {
+              const value = HierParser.parseValue(dataMatched[0].trim(), values)
 
-              if (!selfClosing) openedTagsStack.push(element)
-              str = str.slice(tagMatched.index + tagMatched[0].length)
+              if (value.length) {
+                insertElementToTree(value)
+              }
+              str = str.slice(dataMatched.index + dataMatched[0].length)
             }
-          }
-          state = STATES.data
-          break
+            state = STATES.tag
+            break
+
+          case STATES.tag:
+            const closeMatched = str.match(/^<\/\w+.*>/)
+            if (closeMatched) {
+              str = str.slice(closeMatched.index + closeMatched[0].length)
+              openedTagsStack.pop()
+            } else {
+              const tagMatched = str.match(/<(?<tagName>\w+)[^\s\/>]*\s?(?<attributes>[^>]*)>/)
+              if (tagMatched) {
+                let {
+                  groups: { tagName, attributes },
+                } = tagMatched
+                tagName = HierParser.parseTagName(tagName, values)
+                const selfClosing = attributes.slice(-1) === "/"
+                const props = HierParser.parseProps(attributes, values)
+                const element = { tagName, props }
+                insertElementToTree(element)
+
+                if (!selfClosing) openedTagsStack.push(element)
+                str = str.slice(tagMatched.index + tagMatched[0].length)
+              }
+            }
+            state = STATES.data
+            break
+        }
       }
-    }
 
-    return tokensStack
-  },
+      return tokensStack
+    },
 
-  parseProps(str, values) {
-    str = " " + str.trim()
+    parseProps(str, values) {
+      str = " " + str.trim()
 
-    const props = {}
+      const props = {}
 
-    const matchNextProp = () => {
-      return (
-        /** Double quote */
-        str.match(/ \w+="(?:[^\\"])*"/) ||
-        /** Single quote */
-        str.match(/ \w+='(?:[^\\'])*'/) ||
-        /** Placeholder */
-        str.match(new RegExp(` *\\w+=${HierParser.PLACEHOLDER}`)) ||
-        /** Any other */
-        str.match(/ *\w+/)
-      )
-    }
-
-    let match
-    while ((match = matchNextProp())) {
-      let [key, ...value] = match[0].split("=")
-      key = key.trim()
-
-      value = value.join("=")
-      if (value.match(new RegExp(`["']?${HierParser.PLACEHOLDER}["']?`))) {
-        value = values.shift()
-      } else {
-        value = value ? value.slice(1, -1) : true
+      const matchNextProp = () => {
+        return (
+          /** Double quote */
+          str.match(/ \w+="(?:[^\\"])*"/) ||
+          /** Single quote */
+          str.match(/ \w+='(?:[^\\'])*'/) ||
+          /** Placeholder */
+          str.match(new RegExp(` *\\w+=${HierParser.PLACEHOLDER}`)) ||
+          /** Any other */
+          str.match(/ *\w+/)
+        )
       }
-      if (key.startsWith("on")) key = key.toLowerCase()
 
-      props[key] = value
-      str = str.slice(0, match.index) + str.slice(match.index + match[0].length)
-    }
+      let match
+      while ((match = matchNextProp())) {
+        let [key, ...value] = match[0].split("=")
+        key = key.trim()
 
-    return props
-  },
+        value = value.join("=")
+        if (value.match(new RegExp(`["']?${HierParser.PLACEHOLDER}["']?`))) {
+          value = values.shift()
+        } else {
+          value = value ? value.slice(1, -1) : true
+        }
+        if (key.startsWith("on")) key = key.toLowerCase()
 
-  parseValue(str, values) {
-    while (str.match(new RegExp(HierParser.PLACEHOLDER))) {
-      const value = values.shift()
-
-      if ([null, undefined, ""].includes(value) || !value) {
-        str = str.replace(new RegExp(HierParser.PLACEHOLDER), "")
-      } else {
-        str = str.replace(new RegExp(HierParser.PLACEHOLDER), value)
+        props[key] = value
+        str = str.slice(0, match.index) + str.slice(match.index + match[0].length)
       }
+
+      return props
+    },
+
+    parseTagName(str, values) {
+      if (str.match(new RegExp(HierParser.PLACEHOLDER))) {
+        const value = values.shift()
+
+        return value
+      }
+
+      return str
+    },
+
+    parseValue(str, values) {
+      while (str.match(new RegExp(HierParser.PLACEHOLDER))) {
+        const value = values.shift()
+
+        if ([null, undefined, ""].includes(value) || !value) {
+          str = str.replace(new RegExp(HierParser.PLACEHOLDER), "")
+        } else {
+          str = str.replace(new RegExp(HierParser.PLACEHOLDER), value)
+        }
+      }
+
+      return str
+    },
+  }
+
+  /**
+   * HIER COMPONENTS
+   */
+  class BaseComponent {
+    _prevProps = {}
+    _props = {}
+    props = {}
+    node = null
+
+    constructor(props) {
+      if (props && typeof props !== "object") throw new Error("Please specify correct props object.")
+      this._initChangeableAttr("_props")
+      this._props = props || {}
+      this._prevProps = this._props
+
+      /** Initialisation props object */
+      Object.defineProperty(this, "props", {
+        configurable: false,
+        enumerable: false,
+        get: () => {
+          return this._props || {}
+        },
+        set: (currentValue) => {
+          this._prevProps = this._props
+          this._props = currentValue
+          console.log(`Props was changed from:`, this._prevProps, `to:`, currentValue)
+        },
+      })
     }
 
-    return str
-  },
-}
+    _initChangeableAttr(attr, defaultValue) {
+      Object.defineProperty(this, attr, {
+        configurable: false,
+        enumerable: false,
+        value: defaultValue || {},
+      })
+    }
+
+    render() {
+      return null
+    }
+  }
+
+  class Component extends BaseComponent {
+    _prevState = {}
+    _state = {}
+
+    constructor(props) {
+      super(props)
+      this._initChangeableAttr("_state")
+
+      /** Initialisation state object */
+      Object.defineProperty(this, "state", {
+        configurable: false,
+        enumerable: false,
+        get: () => {
+          return this._state || {}
+        },
+        set: (currentValue) => {
+          this._prevState = this._state
+          this._state = currentValue
+          console.log(`State was changed from:`, this._prevState, `to:`, currentValue)
+        },
+      })
+    }
+
+    /**
+     * @param {object} partialState
+     */
+    setState(partialState) {
+      const prevState = this.state
+      this.state = Object.assign({}, prevState || {}, partialState)
+    }
+  }
+
+  return {
+    render: Hier.render,
+    html: HierParser.ast,
+    BaseComponent,
+    Component,
+  }
+})()
--- jsx/script.js
+++ jsx/script.js
@@ -1,69 +1,60 @@
 "use strict"
 
-const { createElement } = Hier
-const app = createElement("form", null, [
-  createElement("fieldset", null, [
-    createElement("legend", null, "Register Form"),
-    createElement("main", null, [
-      createElement("div", null, [
-        createElement("label", null, [
-          "First Name: ",
-          createElement("input", { type: "text", placeholder: "First Name" }),
-        ]),
-      ]),
-      createElement("div", null, [
-        createElement("label", null, [
-          "Last Name: ",
-          createElement("input", { type: "text", placeholder: "Last Name" }),
-        ]),
-      ]),
-      createElement("div", null, [
-        createElement("label", null, ["Age: ", createElement("input", { type: "number", placeholder: "Age", min: 0 })]),
-      ]),
-    ]),
-    createElement("footer", null, [createElement("button", { type: "button" }, "Register")]),
-  ]),
-])
-document.querySelector("#app").appendChild(app)
-
 /**
  *
  */
-console.time("HTML parsing")
-const { html } = HierParser
-const buttonText = `Lets register!`
-const app2 = html`<form action="/some/cool/url" method="post">
-  <fieldset>
-    <legend>Register Form</legend>
-    <main>
-      <div>
-        <label>
-          First Name:
-          <input type="text" placeholder="First Name" />
-        </label>
-      </div>
+console.time()
+const { html, Component, BaseComponent } = Hier
 
-      <div>
-        <label>
-          Last Name:
-          <input type="text" placeholder="Last Name" />
-        </label>
-      </div>
+class UserName extends BaseComponent {
+  render() {
+    return html`<span class="username">${this.props.name}</span>`
+  }
+}
 
-      <div>
-        <label>
-          Age:
-          <input type="number" placeholder="Age" min="0" />
-        </label>
-      </div>
-    </main>
+class Hello extends BaseComponent {
+  render() {
+    return html`Hello, <${UserName} name=${this.props.name || "(noname)"} />`
+  }
+}
 
-    <footer>
-      <button type="button">${buttonText}</button>
-    </footer>
-  </fieldset>
-</form>`
-console.timeEnd("HTML parsing")
+class App extends Component {
+  render() {
+    const buttonText = `Lets register!`
+    const setFirstName = (e) => this.setState({ firstName: e.target.value })
+    const setLastName = (e) => this.setState({ lastName: e.target.value })
+    const clickButton = () => alert("Okay, lets go!")
 
-console.log(app2)
-app2.map((element) => document.querySelector("#app").appendChild(element))
+    return html`Before Application Test
+
+      <div>Yo 1: <${Hello} /></div>
+      Yo 2: <${Hello} />
+
+      <form action="/some/cool/url" method="post">
+        <fieldset>
+          <legend>Register Form</legend>
+          <main>
+            <div>
+              <input type="text" placeholder="First Name" onInput=${setFirstName} />
+              <input type="text" placeholder="Last Name" onInput=${setLastName} />
+            </div>
+
+            <div>
+              <label>
+                Age:
+                <input type="number" placeholder="Age" min="0" />
+              </label>
+            </div>
+          </main>
+
+          <footer>
+            <button type="button" onClick=${clickButton}>${buttonText}</button>
+          </footer>
+        </fieldset>
+      </form>
+      After Application Text`
+  }
+}
+
+Hier.render(App, document.getElementById("app"))
+console.timeEnd()
