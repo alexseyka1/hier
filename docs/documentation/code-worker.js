0037b2f4f81a1eb465c88f9f7f6ea1f433b27b10
alexseyka1 <alexseyka1@gmail.com>
2022-04-25 15:46:20 +0300

    fixed code highlighting worker

--- docs/documentation/code-worker.js
+++ docs/documentation/code-worker.js
@@ -1,6 +1,6 @@
+importScripts("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/highlight.min.js")
+importScripts("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/languages/javascript.min.js")
 onmessage = (event) => {
-  importScripts("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/highlight.min.js")
-  importScripts("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/languages/javascript.min.js")
   const { text, index } = event.data
   const result = self.hljs.highlightAuto(text)
   postMessage({ result: result.value, index })
